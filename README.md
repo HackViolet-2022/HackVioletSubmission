eea456552c29c070b523b96df2c213e461fc0dde
ChengAaron <cheng.aaron@outlook.com>
2022-02-05 17:21:13 -0500

    Initial push

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # HackVioletSubmission
+
+Feeling down? Need a pick me up? We've created a discord chat bot that would help to cheer you up!
---
+++ keys.py
@@ -0,0 +1,6 @@
+key1 = open("discordKey.txt", "r")
+key2 = open("openAiKey.txt", "r")
+
+DISCORD_KEY = key1.read()
+OPENAPI_KEY = key2.read()
+
