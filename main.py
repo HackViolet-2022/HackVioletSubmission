5f87de6afa9f53693825944af9f4e52104efa559
dapdaw <76751370+dapdaw@users.noreply.github.com>
2022-02-05 17:17:35 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,26 @@
 import openai
-import os
 import discord
+from discord.ext import commands
+from discord.ext.commands import command
 
-line = open("key.txt","r")
+discordToken = open("discordKey.txt","r").read()
+openAItoken = open("openAiKey.txt","r").read()
+bot = commands.Bot(command_prefix = '!')
+
+@bot.event
+async def on_ready():
+    print("ready for action")
+
+@bot.command()
+async def hello(ctx):
+    await ctx.send("Hello There!")
+
+@bot.event
+async def on_message(message):
+    channel = message.channel
+    if message.channel.id == message.author.dm_channel.id:
+        print("Hello")
+        await channel.send("Hello")
+
+
+bot.run(discordToken)
