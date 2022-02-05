748de41f1a524201d389cfbd4dd4ea95ec704fb6
dapdaw <76751370+dapdaw@users.noreply.github.com>
2022-02-05 18:13:58 -0500

    Update main.py

--- main.py
+++ main.py
@@ -5,8 +5,21 @@ from discord.ext import commands
 from discord.ext.commands import command
 import keys
 
-openai.api_key = os.getenv(keys.OPENAPI_KEY)
+#Initialize bots
 bot = commands.Bot(command_prefix = '!')
+openai.api_key = os.getenv(keys.OPENAPI_KEY)
+
+def talk(message):
+    response = openai.Completion.create(
+        engine="text-ada-001",
+        prompt = message,
+        temperature = 0.5,
+        max_tokens = 500,
+        top_p = 1,
+        frequency_penalty = 0,
+        presence_penalty = 0
+    )
+
 
 @bot.event
 async def on_ready():
@@ -25,6 +38,7 @@ async def on_message(message):
     if message.channel.id == message.author.dm_channel.id:
         print("Hello")
         await channel.send("Hello")
+    
 
 
 bot.run(keys.DISCORD_KEY)
