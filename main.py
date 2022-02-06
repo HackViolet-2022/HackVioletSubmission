533728e26452f0b48d740ffd0b505bd432e7321d
dapdaw <76751370+dapdaw@users.noreply.github.com>
2022-02-05 19:46:22 -0500

    Update main.py

--- main.py
+++ main.py
@@ -7,18 +7,25 @@ import keys
 
 #Initialize bots
 bot = commands.Bot(command_prefix = '!')
-openai.api_key = os.getenv(keys.OPENAPI_KEY)
 
+
+#This function will reply based on message sent from user (exapnd with MongoDB later for optimization)
 def talk(message):
+    openai.api_key = keys.OPENAPI_KEY
     response = openai.Completion.create(
         engine="text-ada-001",
         prompt = message,
-        temperature = 0.5,
-        max_tokens = 500,
+        temperature = 0.8,
+        max_tokens = 200,
         top_p = 1,
         frequency_penalty = 0,
         presence_penalty = 0
     )
+    content = response.choices[0].text.split('.')
+    print(content)
+    return response.choices[0].text
+
+
 
 
 @bot.event
@@ -37,7 +44,19 @@ async def on_message(message):
     channel = message.channel
     if message.channel.id == message.author.dm_channel.id:
         print("Hello")
-        await channel.send("Hello")
+        await channel.send("Hello") 
+        x = message.author.id #store message author
+        print(message.content)
+        await channel.send(talk(message.content))
+        
+
+        #check if its in db, if not store it
+        #store id into mongo db
+        #call talk(message), store the query + answer, talk returns (optimize)
+
+        #if new user:
+            #do some probing 
+
     
 
 
